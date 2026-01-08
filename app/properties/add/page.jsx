3555ad6dae9d439f92744efb4dafa55b103b41f7
ahmedmabood <ahmedmabood12@gmail.com>
2026-01-08 23:54:45 +0500

    fix issues in Navbar.jsx

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,18 +1,20 @@
-const PropertyAddpage = () => {
+import PropertyAddForm from '@/components/propertyAddForm'
+
+const PropertyAddPage = () => {
     return (
     <section className="bg-blue-50">
       <div className="container m-auto max-w-2xl py-24">
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
-          <PropertyAddpage />
+          <PropertyAddForm />
         </div>
       </div>
     </section>)
 
 }
 
-export default PropertyAddpage
+export default PropertyAddPage
 
 
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
-              onClick={()=>isMobileMenuOpen((prev)=>!prev)}
+              onClick={()=>setIsMobileMenuOpen((prev)=>!prev)}
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
